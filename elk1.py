b156b35fa6fd9f91b91f00ada1a5339b82e3993c
Junhyung Ki <kijoonh91@gmail.com>
2021-06-25 22:06:01 +0200

    elk1 python client updated, kibana example query commands added

--- elk1.py
+++ elk1.py
@@ -11,56 +11,105 @@ try:
     import os
     import sys
     import numpy as np
-    print("Loaded ...")
+    print('Loaded ...')
 except Exception as e:
-    print("Some Modules are Missing {} ".format(e))
+    print('Some Modules are Missing {} '.format(e))
 
-def generator(df2):
-    for c, line in enumerate(df2):
+def generator(contents):
+    for content in contents:
+        # specifying type and id in bulk requests is deprecated (7.13.2)
+        # all documents will just be sent to elasticsearch to be indexed as-is
         yield {
             '_index': 'myelkfirst',
             '_type': '_doc',
-            '_id': line.get("show_id", None),
-            '_source': { # source is where you define the JSON document
-                'title': line.get('title', ''),
-                'director': line.get('director', ''),
-                'description': line.get('description', ''),
-                'duration': line.get('duration', None),
-                'cast': line.get('cast', None)
+            '_id': content.get('show_id', ''),
+            '_source': {
+                # source is where you define the JSON document
+                'title': content.get('title', ''),
+                'director': content.get('director', ''),
+                'description': content.get('description', ''),
+                'duration': content.get('duration', ''),
+                'cast': content.get('cast', '')
             }
         }
+        # refer to https://elasticsearch-py.readthedocs.io/en/v7.13.2/helpers.html#example
     raise StopIteration
 
 # Read all the files in the current directory
 for x in os.listdir():
     print(x)
-df = pd.read_csv("netflix_titles.csv")
+df = pd.read_csv('netflix_titles.csv')
 print(df.head(3)) # show the top 3 contents
 print(df.columns) # show all columns used in the index
 print(df.shape) # how many rows and columns
-print(df["show_id"].nunique())
-print("\n ##### \n")
+print(df['show_id'].nunique())
+print('\n ##### \n')
 
 # Read the Dataset
-df = pd.read_csv("netflix_titles.csv")
+df = pd.read_csv('netflix_titles.csv')
 
 # Create an Elasticsearch instance
-ENDPOINT = "http://localhost:9200/" # if you are using an AWS instance you can replace this with it
+ENDPOINT = 'http://localhost:9200/' # if you are using an AWS instance you can replace this with it
 es = Elasticsearch(timeout=600, hosts=ENDPOINT)
 print(es.ping())
-print("\n ##### \n")
+print('\n ##### \n')
 
 # Before uploading, do some data cleaning
 print(df.isna().sum()) # how many null values are there
 df = df.dropna() # drop all the documents with null value(s), this is how you clean data
 print(df.isna().sum())
 print(df.shape) # how many rows and columns
+print('\n ##### \n')
 
 # You need to convert the data into appropriate format that Elasticsearch can understand
-df2 = df.to_dict('records') # dataframe into a dictionary of series ('dict', 'list', 'series', 'split', 'records', 'index')
-print(df2[0])
-print(len(df2))
+contents = df.to_dict('records') # dataframe into a dictionary of series ('dict', 'list', 'series', 'split', 'records', 'index')
+print(contents[0])
+print(len(contents))
+print('\n ##### \n')
 
-# You need to convert the data into ELK format
-mycustom = generator(df2)
-print(json.dumps(next(mycustom), indent=3)) # pretty
+# # You need to convert the data into ELK format
+# mycustom = generator(contents)
+# for i in range(len(contents)):
+#     print(json.dumps(contents[i], indent=3) + '\n')
+# print('done')
+# #print(json.dumps(next(mycustom), indent=3)) # pretty
+
+# Settings and Mappings
+settings = {
+    'settings': {
+        'number_of_shards': 1,
+        'number_of_replicas': 0
+    },
+    'mappings': {
+        'properties': {
+            'director': {
+                'type': 'text',
+            },
+            'duration': {
+                'type': 'text',
+            }
+        }
+    }
+}
+
+# Upload the data into Elasticsearch
+try:
+    res = helpers.bulk(es, generator(contents))
+    print('Working')
+except Exception as e:
+    print('Not working {} '.format(e))
+    pass
+
+# Delete the index
+es.indices.delete(index='myelkfirst', ignore=[400, 404])
+
+# Create the index
+my = es.indices.create(index='myelkfirst', ignore=[400, 404], body=settings)
+print(my)
+
+try:
+    res = helpers.bulk(es, generator(contents))
+    print('Working')
+except Exception as e:
+    print('Not working {} '.format(e))
+    pass
